7f5aa58494153f4256e36a42bf4cf223ad59299f
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-08-23 13:30:03 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Container from './components/Container'
+import Footer from './components/Footer'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+import Dice from './Dice'
+
+export default function Container({ children }) {
+    return (
+        <div>
+            <Dice />
+        </div>
+    )
+}
