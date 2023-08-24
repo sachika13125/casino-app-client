44b599fbebf956ebb256c975adb07223a37d8024
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-08-23 20:14:56 -0700

    modify score display

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function Dice() {
 
@@ -6,19 +6,22 @@ export default function Dice() {
   const [guess, setGuess] = useState('');
   const [gameOver, setGameOver] = useState(false);
   const [previousRoll, setPreviousRoll] = useState(null);
+  const [score, setScore] = useState(0);
 
   const updateRemainingGuesses = () => setRemainingGuesses(remainingGuesses - 1);
 
   const rollDice = () => {
     const roll = Math.floor(Math.random() * 6) + 1;
     setPreviousRoll(roll);
+    return roll;
   };
 
   const handleRollClick = () => {
     if (!gameOver && remainingGuesses > 0) {
       const roll = rollDice();
+      console.log(guess)
       if (parseInt(guess) === roll) {
-        updateScore();
+        setScore(score + 20);
       }
       updateRemainingGuesses();
       if (remainingGuesses === 1) {
@@ -35,7 +38,15 @@ export default function Dice() {
     setGameOver(false);
   };
 
-
+//   const updateScore = () => {
+//     fetch('/getDiceRewards')
+//       .then(response => response.json())
+//       .then(data => {
+//         const scoreFromServer = data.length > 0 ? data[0].reward : 0;
+//         setScores(scores + scoreFromServer);
+//       })
+//       .catch(error => console.error('Error fetching score:', error));
+//   };
 
 
     return(
@@ -44,10 +55,12 @@ export default function Dice() {
             <div id="rewards-container" className="bg-gray-100 p-4 rounded-lg mb-4 w-1/2 mx-auto"></div>
             <div id="game" className="p-4 rounded-lg">
                 <p className='text-white'>Remaining Guesses: <span id="remaining-guesses">{remainingGuesses}</span></p>
-                <p className='text-white'>Score: <span id="score">0</span></p>
+                <p className='text-white'>Score: <span id="score" className='text-lg font-bold'>{score}</span></p>
                 <p className='text-white flex justify-center items-center'>Previous Roll: {previousRoll !== null ? <img src={`/images/dice${previousRoll}.png`} alt={`Dice Roll ${previousRoll}`} className='w-1/4 text-center' /> : "-"}</p>                
                 <label htmlFor="guess" className="block text-white text-sm font-bold mb-2">Enter your guess (1-6):</label>
-                <input type="number" id="guess" min="1" max="6" className="block w-full p-2 border rounded-md w-1/6 mx-auto mb-4" />
+                <input type="number" id="guess" min="1" max="6" value={guess}
+                        onChange={event => setGuess(event.target.value)} 
+                        className="block w-full p-2 border rounded-md w-1/6 mx-auto mb-4" />
                 <div className='flex justify-around'>
                 <button id="roll-button" 
                         onClick={handleRollClick} 
