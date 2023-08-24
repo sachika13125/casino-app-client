e6784ed83700339edf115947c220f7e847d3a227
KennyAle <kennyviver@gmail.com>
2023-08-23 23:50:04 -0700

    style

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
     <div>
       {/* <Header /> */}
       <Container />
-      <h1>Test</h1>
       {/* <Footer /> */}
     </div>
   )
--- src/components/Blackjack.jsx
+++ src/components/Blackjack.jsx
@@ -16,7 +16,6 @@ export default function Blackjack() {
     useEffect(() => {
         buildDeck();
         shuffleDeck();
-        startGame();
     }, []);
 
     return (
